224f2c50f161a9ec62e5b3af36b2ba21497da73b
vineet-alampally <48998765+vineet-alampally@users.noreply.github.com>
2022-11-20 19:52:54 -0500

    added dislike

--- src/tuiter/tuits/tuitstats.js
+++ src/tuiter/tuits/tuitstats.js
@@ -22,23 +22,36 @@ const TuitStats = ({stats}) => {
                                      likes: stats.likes + x
                                  }));
     }
+    const dislikeClickHandler = () => {
+        dispatch(updateTuitThunk({
+                                     ...stats,
+                                     dislikes: stats.dislikes + 1
+                                 }));
+    }
 
-    return (<div className="row mt-2 ms-5">
-        <div className="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3">
+    return (
+        <div className="row mt-2 ms-5">
+            <div className="col-xxl-2 col-xl-2 col-lg-2 col-md-2 col-sm-2">
             <i className='fa fa-regular fa-comment wd-gray-color'></i>
             <span className='ms-1 wd-gray-color'>{stats.replies}</span>
         </div>
-        <div className="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3">
+            <div className="col-xxl-2 col-xl-2 col-lg-2 col-md-2 col-sm-2">
             <i className='fa fa-solid fa-retweet wd-gray-color'></i>
             <span className='ms-1 wd-gray-color'>{stats.retuits}</span>
         </div>
-        <div className="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3">
+            <div className="col-xxl-2 col-xl-2 col-lg-2 col-md-2 col-sm-2">
             <i onClick={likeClickHandler}
                className='fa fa-heart'
                style={{"color": `${isActive ? "red" : "gray"}`}}></i>
             <span className='ms-1 wd-gray-color'>{stats.likes}</span>
         </div>
-        <div className="col-xxl-3 col-xl-3 col-lg-3 col-md-3 col-sm-3">
+            <div className="col-xxl-2 col-xl-2 col-lg-2 col-md-2 col-sm-2">
+            <i onClick={dislikeClickHandler}
+               className="fa-solid fa-thumbs-down"
+               style={{"color": "gray"}}></i>
+            <span className='ms-1 wd-gray-color'>{stats.dislikes}</span>
+        </div>
+        <div className="col-xxl-2 col-xl-2 col-lg-2 col-md-2 col-sm-2">
             <i className='fa-solid fa-share-nodes wd-gray-color'></i>
 
         </div>
