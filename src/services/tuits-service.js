ef5e106b30e5e6dcd10c952b900e651df3e23146
vineet-alampally <alampally.v@northeastern.edu>
2022-11-30 20:49:09 -0500

    a9  changes

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -11,6 +11,7 @@ export const findTuits = async () => {
     const response = await axios.get(TUITS_API);
     const tuits = response.data;
     return tuits;
+    
 }
 
 export const deleteTuit = async (tid) => {
